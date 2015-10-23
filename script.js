dda33e041bbf6e65e2037e6781785616a4a3f9d2
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-10-23 13:29:11 -0400

    alt save 2

--- script.js
+++ script.js
@@ -10,31 +10,25 @@ for (var i = 1; i<=9; i++) {
     $(".game").append(gameBoard);
   }
 
-var clickcount = 1;
+var clickCount = true;
 function gamePlay(){
   console.log("clicked")
-  clickcount++;
-  if (clickcount%2===0) {
+  // clickCount = true;
+  if (clickCount===true) {
   $(this).html("x");
-  $(this).off("click");
+  clickCount=false;
 } else {
   $(this).html("o");
-  $(this).off("click");
+  clickCount=true;
 }
 }
 
 $(".squares").on("click", gamePlay);
 
 $(".resetButton").on("click", function (){
-  resetCount = true;
-  if (resetCount === true) {
   $(".squares").html("");
-  clickcount = 1;
   $(".squares").on("click", gamePlay);
-  resetCount= false;
-
 }
-
-})
+)
 
 })
